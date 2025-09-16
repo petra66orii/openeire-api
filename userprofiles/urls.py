56faa536d2a8a3618cc022cd670b8781c8513c57
Petra <petra66orii@gmail.com>
2025-09-16 20:47:13 +0100

    Add URLs for password reset, #4

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmailView
+from .views import RegisterView, VerifyEmailView, PasswordResetRequestView, PasswordResetConfirmView
 from rest_framework_simplejwt.views import (        
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,4 +10,6 @@ urlpatterns = [
     path('verify-email/confirm/', VerifyEmailView.as_view(), name='auth_verify_email'),
     path('login/', TokenObtainPairView.as_view(), name='auth_login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password/reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
