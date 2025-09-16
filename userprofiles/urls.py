e678b70040494aabcf406db2838cfcbe6d667f9b
Petra <petra66orii@gmail.com>
2025-09-16 19:32:19 +0100

    Add URLs for email verification and login, #2, #3

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, VerifyEmailView
+from rest_framework_simplejwt.views import (        
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('verify-email/confirm/', VerifyEmailView.as_view(), name='auth_verify_email'),
+    path('login/', TokenObtainPairView.as_view(), name='auth_login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
