8689637e1e957fc97cba58c78a6be276f9cf25b0
Petra <petra66orii@gmail.com>
2025-09-19 17:55:35 +0100

    Add necessary URLs for product detail page, #9

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import GalleryListView
+from .views import GalleryListView, PhotoDetailView, VideoDetailView, ProductDetailView
 
 urlpatterns = [
     path('gallery/', GalleryListView.as_view(), name='gallery_list'),
+    path('photos/<int:pk>/', PhotoDetailView.as_view(), name='photo_detail'),
+    path('videos/<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
 ]
