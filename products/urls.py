38f3cda9d5ce17f05b263b673278952e5b21c89a
Petra <petra66orii@gmail.com>
2025-09-22 21:59:43 +0100

    Add URL for product reviews, #13

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import GalleryListView, PhotoDetailView, VideoDetailView, ProductDetailView, ProductReviewCreateView
+from .views import (
+    GalleryListView,
+    PhotoDetailView,
+    VideoDetailView,
+    ProductDetailView,
+    ProductReviewListCreateView
+)
 
 urlpatterns = [
     path('gallery/', GalleryListView.as_view(), name='gallery_list'),
     path('photos/<int:pk>/', PhotoDetailView.as_view(), name='photo_detail'),
     path('videos/<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-     path('<str:product_type>/<int:pk>/reviews/', ProductReviewCreateView.as_view(), name='review_create'),
+    path('<str:product_type>/<int:pk>/reviews/', ProductReviewListCreateView.as_view(), name='review_list_create'),
 ]
