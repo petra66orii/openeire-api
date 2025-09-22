07de331005fb30f763c585baf1e247ff7ebb2406
Petra <petra66orii@gmail.com>
2025-09-22 21:59:22 +0100

    Move comment_snippet function above product_link function

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Photo, Video, Product, ProductReview
 from django.utils.html import format_html
+from django.urls import reverse
 
 admin.site.register(Photo)
 admin.site.register(Video)
@@ -12,25 +13,27 @@ class ProductReviewAdmin(admin.ModelAdmin):
     list_display = ('product_link', 'user', 'rating', 'comment_snippet', 'approved', 'created_at')
     list_filter = ('approved', 'rating', 'created_at')
     search_fields = ('comment', 'user__username', 'product__title')
+    
+    # Custom method to display a snippet of the comment - MOVED HERE
+    def comment_snippet(self, obj):
+        return obj.comment[:75] + '...' if len(obj.comment) > 75 else obj.comment
+    comment_snippet.short_description = 'Comment'
+
     actions = ['mark_as_approved', 'mark_as_unapproved']
 
     # Custom method to display product title and link to its admin page
     def product_link(self, obj):
         if obj.product:
-            # Get the admin URL for the specific product type
             content_type = obj.content_type
-            product_admin_url = admin.site.reverse_admin_url(
-                f'{content_type.app_label}_{content_type.model}_change',
+            
+            url = reverse(
+                f'admin:{content_type.app_label}_{content_type.model}_change',
                 args=[obj.object_id]
             )
-            return format_html('<a href="{}">{}</a>', product_admin_url, obj.product.title)
+            return format_html('<a href="{}">{}</a>', url, obj.product.title)
         return "N/A"
     product_link.short_description = 'Product'
 
-    # Custom method to display a snippet of the comment
-    def comment_snippet(self, obj):
-        return obj.comment[:75] + '...' if len(obj.comment) > 75 else obj.comment
-    comment_snippet.short_description = 'Comment'
 
     # Custom action: Mark selected reviews as approved
     @admin.action(description='Mark selected reviews as approved')
