8fc3ee8de48e8c4540a373e13b4d775c8f8d9e87
Petra <petra66orii@gmail.com>
2025-09-23 18:37:57 +0100

    Create Order and OrderItem models and migrate, #15

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,62 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
 
-# Create your models here.
+from userprofiles.models import UserProfile
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django_countries.fields import CountryField
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
+    first_name = models.CharField(max_length=150, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=255, null=False, blank=False)
+    street_address2 = models.CharField(max_length=255, null=True, blank=True)
+    town = models.CharField(max_length=100, null=False, blank=False)
+    county = models.CharField(max_length=100, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='items')
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+    item_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+
+    # Generic ForeignKey to link to a Photo, Video, or Product
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    product = GenericForeignKey('content_type', 'object_id')
+    
+    # Store details at time of purchase
+    details = models.JSONField(null=True, blank=True) # e.g., {'quality': '4k'} or {'size': 'A4'}
+
+    def __str__(_self):
+        return f"Item for order {_self.order.order_number}"
