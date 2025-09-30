ebb9ec2f2e65809a6ed01ff90582ba983e745558
Petra <petra66orii@gmail.com>
2025-09-30 17:38:44 +0100

    Register testimonial model with admin, #25

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Testimonial
+from openeire_api.admin import custom_admin_site
 
-# Register your models here.
+@admin.register(Testimonial, site=custom_admin_site)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rating', 'text')
