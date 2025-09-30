b51dbd1fe43ab5e87203b8471fb9180bffaa7406
Petra <petra66orii@gmail.com>
2025-09-30 17:36:49 +0100

    Create testimonial model and migrate, #25

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Testimonial(models.Model):
+    """
+    Model to represent a client testimonial.
+    """
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    rating = models.PositiveIntegerField(default=5)
+
+    def __str__(self):
+        return f"Testimonial by {self.name}"
