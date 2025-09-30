05ee0c78065eb04a71f75b858be2a29c298455ed
Petra <petra66orii@gmail.com>
2025-09-30 20:28:07 +0100

    Register newletter model with admin, #26

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Testimonial
+from .models import Testimonial, NewsletterSubscriber
 from openeire_api.admin import custom_admin_site
 
 @admin.register(Testimonial, site=custom_admin_site)
 class TestimonialAdmin(admin.ModelAdmin):
     list_display = ('name', 'rating', 'text')
+
+
+@admin.register(NewsletterSubscriber, site=custom_admin_site)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'created_at')
