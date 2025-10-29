e53e9802065ba372c4b41a09fce778aeae04bcdf
Petra <petra66orii@gmail.com>
2025-10-29 18:51:51 +0000

    Register model

--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_country')
+    search_fields = ('user__username', 'user__email')
