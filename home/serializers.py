e57ed42eb643f20853013c9831a78ca9d1ddc454
Petra <petra66orii@gmail.com>
2025-12-16 17:05:15 +0000

    Create serializer for contact page, #28

--- home/serializers.py
+++ home/serializers.py
@@ -10,3 +10,14 @@ class NewsletterSubscriberSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsletterSubscriber
         fields = ('id', 'email')
+
+
+class ContactFormSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
+    subject = serializers.CharField(max_length=200)
+    message = serializers.CharField(max_length=2000)
+
+    def validate_subject(self, value):
+        # Optional: Add specific logic if you want to restrict subjects
+        return value
