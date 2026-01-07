232c95be43005462e29517c2597ace58d768a7e0
Petra <petra66orii@gmail.com>
2026-01-07 17:00:00 +0000

    Change Stripe webhook URL

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,6 +3,6 @@ from .views import CreatePaymentIntentView, StripeWebhookView, OrderHistoryView
 
 urlpatterns = [
     path('create-payment-intent/', CreatePaymentIntentView.as_view(), name='create_payment_intent'),
-    path('webhook/', StripeWebhookView.as_view(), name='stripe_webhook'),
+    path('wh/', StripeWebhookView.as_view(), name='webhook'),
     path('order-history/', OrderHistoryView.as_view(), name='order_history'),
 ]
