c30cba4173e63fec976406a5f027d4d558bdeb84
Petra <petra66orii@gmail.com>
2026-01-16 16:51:59 +0000

    Add likes URL to file, #40

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogPostListView, BlogPostDetailView, CommentListCreateView
+from .views import BlogPostListView, BlogPostDetailView, CommentListCreateView, BlogPostLikeView
 
 urlpatterns = [
-    path('posts/', BlogPostListView.as_view(), name='blog_post_list'),
-    path('posts/<slug:slug>/', BlogPostDetailView.as_view(), name='blog_post_detail'),
-    path('posts/<slug:slug>/comments/', CommentListCreateView.as_view(), name='comment_list_create'),
+    path('', BlogPostListView.as_view(), name='blog_post_list'),
+    path('<slug:slug>/', BlogPostDetailView.as_view(), name='blog_post_detail'),
+    path('<slug:slug>/like/', BlogPostLikeView.as_view(), name='blog_like'),
+    path('<slug:slug>/comments/', CommentListCreateView.as_view(), name='comment_list_create'),
 ]
