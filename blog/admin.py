7e91294b0fb38c097acad7d0729a4f09d1f1930f
Petra <petra66orii@gmail.com>
2026-01-16 16:52:27 +0000

    Fix blog post display in admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import BlogPost, Comment
 from openeire_api.admin import custom_admin_site
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @admin.register(BlogPost, site=custom_admin_site)
 class BlogPostAdmin(SummernoteModelAdmin):
@@ -9,10 +12,22 @@ class BlogPostAdmin(SummernoteModelAdmin):
     Admin configuration for the BlogPost model.
     """
     list_display = ('title', 'slug', 'author', 'status', 'created_at')
-    list_filter = ('status', 'created_at', 'author')
+    list_filter = ('status', 'created_at', 'author', 'tags')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+    readonly_fields = ('likes_count_display',) 
+    exclude = ('likes',) 
+
+    def likes_count_display(self, obj):
+        return obj.number_of_likes()
+    likes_count_display.short_description = 'Total Likes'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "author":
+            # Only show users where is_superuser=True
+            kwargs["queryset"] = User.objects.filter(is_superuser=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 @admin.register(Comment, site=custom_admin_site)
 class CommentAdmin(admin.ModelAdmin):
