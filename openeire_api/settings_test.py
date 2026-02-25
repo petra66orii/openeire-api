34907c4a28680e37c8e6b1d8ce26f813648ae037
Miss Bott <developer@missbott.online>
2026-02-25 14:49:45 +0000

    Add test settings to pass CI

---
+++ openeire_api/settings_test.py
@@ -0,0 +1,11 @@
+from .settings import *  # noqa
+
+# Force local file storage for tests (avoid R2/S3 calls)
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"},
+}
+
+MEDIA_ROOT = BASE_DIR / "test_media"
